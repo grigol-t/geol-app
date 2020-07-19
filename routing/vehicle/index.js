fad2c26c2f3074e1730251797833a5ed8117bfca
Grigol <grigol.tchkoidze@gmail.com>
2020-07-19 21:08:30 +0400

    updates

--- routing/vehicle/index.js
+++ routing/vehicle/index.js
@@ -1,12 +1,11 @@
-"user strict";
+'user strict';
 
-const Router = require("express").Router;
-const ctrl = require("./controller");
+const Router = require('express').Router;
+const ctrl = require('./controller');
 const router = Router();
 
-router.get("/vehicle/personal/:id", ctrl.findById);
-router.get("/vehicle/list", ctrl.list);
-router.put("/vehicle/create", ctrl.create);
-
+router.get('/vehicle/personal/:id', ctrl.findById);
+router.get('/vehicle/list', ctrl.list);
+router.put('/vehicle/create', ctrl.create);
 
 module.exports = router;
